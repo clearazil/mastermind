44e41259e53a2eae3ce33184d12c221c551a209f
dvanderh <derkvanderheide@hotmail.com>
2020-01-29 14:28:06 +0100

    implement game mechanics

--- js/classes/mastermind.js
+++ js/classes/mastermind.js
@@ -6,6 +6,7 @@ import MastermindDom from './mastermind-dom';
 export default class Mastermind {
     _dom = new MastermindDom;
     _currentRowId;
+    _colorsToGuess;
 
     /**
      * @return {MastermindDom}
@@ -17,7 +18,7 @@ export default class Mastermind {
     /**
      * @return {array}
      */
-    get validColors() {
+    get codeColors() {
         return [
             'blue',
             'green',
@@ -43,10 +44,40 @@ export default class Mastermind {
     }
 
     /**
+     * @param {array} colors
+     */
+    set codeToGuess(colors) {
+        this._colorsToGuess = colors;
+    }
+
+    /**
+     * @return {array}
+     */
+    get codeToGuess() {
+        return this._colorsToGuess;
+    }
+
+    /**
+     * Generate a code of 4 colors
+     */
+    generateColorCode() {
+        let colorKeyIndex;
+        const codeToGuess = [];
+
+        for (let i = 1; i <= 4; i++) {
+            colorKeyIndex = Math.round(Math.random() * 5);
+            codeToGuess.push(this.codeColors[colorKeyIndex]);
+        }
+
+        this.codeToGuess = codeToGuess;
+    }
+
+    /**
      *
      */
     createNewGame() {
         this._dom.createNewBoard();
+        this.generateColorCode();
         this.currentRowId = 1;
     }
 
@@ -58,12 +89,12 @@ export default class Mastermind {
         const colors = this.dom.getChosenColors();
 
         if (this.isColorsValid(colors)) {
-            if (this.isCorrectGuess()) {
+            this.dom.displayKeyPegs(this.getKeyPegHintColors(colors));
+
+            if (this.isCorrectGuess(colors)) {
                 this.dom.displayGameWonMessage();
             } else {
                 this.currentRowId++;
-
-                this.dom.displayKeyPegs(this.getKeyPegHintColors());
                 this.dom.advanceToNextRow(this.currentRowId);
             }
         } else {
@@ -72,23 +103,63 @@ export default class Mastermind {
     }
 
     /**
+     * Get the amount of red and
+     * white hint pegs to give
+     * @param {array} rowColors
      * @return {object}
      */
-    getKeyPegHintColors() {
-        // TODO
-        return {
-            red: 1,
-            white: 1,
+    getKeyPegHintColors(rowColors) {
+        // avoid changing the rowColors variable
+        const colors = rowColors.slice(0);
+
+        const pegHints = {
+            red: 0,
+            white: 0,
         };
+
+        const incorrectPositions = [];
+        const colorsNotGuessed = [];
+
+        for (let i = 0; i < colors.length; i++) {
+            if (this.codeToGuess[i] === colors[i]) {
+                colors[i] = null;
+                pegHints.red++;
+            } else {
+                incorrectPositions.push(i);
+                colorsNotGuessed.push(this.codeToGuess[i]);
+            }
+        }
+
+        for (let i = 0; i < colors.length; i++) {
+            const isInWrongPlace = colorsNotGuessed.includes(colors[i]);
+
+            if (incorrectPositions.includes(i) && isInWrongPlace) {
+                pegHints.white++;
+
+                // delete the color, to ensure white key
+                // pegs are not awarded for one color twice
+                const index = colorsNotGuessed.indexOf(colors[i]);
+                colorsNotGuessed.splice(index, 1);
+            }
+        }
+
+        return pegHints;
     }
 
     /**
      * Check if the player has correctly
      * guessed the four colors
+     * @param {array} colors
      * @return {bool}
      */
-    isCorrectGuess() {
-        return false; // TODO
+    isCorrectGuess(colors) {
+        for (let i = 0; i < colors.length; i++) {
+            if (this.codeToGuess[i] !== colors[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
@@ -101,7 +172,7 @@ export default class Mastermind {
      */
     isColorsValid(colors) {
         for (let i = 0; i < colors.length; i++) {
-            if (this.validColors.includes(colors[i]) === false) {
+            if (this.codeColors.includes(colors[i]) === false) {
                 return false;
             }
         }
